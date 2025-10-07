8ac777beac2a2632f01bc8b1a14add2604aca05d
Sebastiano Silipo <sebastiano.silipo98@gmail.com>
2025-10-07 17:41:50 +0900

    [#FIX] clickable time on header

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -9,8 +9,8 @@ const NavBar = () => {
     return(
         <Row className='justify-content-center align-items-center nav-container pb-md-3 pb-1 pt-md-3 pt-1'>
             <Col className='logo'>
-                <Link to="/" className="nav-item">Sebastiano's Portfolio</Link>
-                <CurrentTime/>
+                <Link to="/" className="nav-item">Sebastiano's Portfolio
+                <CurrentTime/></Link>
             </Col>
             <Col className='d-flex flex-row-reverse'>
                 <Button href={CV_Definitivo} download="Resume_PDF" target='_blank' rel="noferrer" className='cta-btn' variant="warning">Resume</Button>
