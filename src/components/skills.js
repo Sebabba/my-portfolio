cf42eaffe091e9869238444f971787fba040d919
Sebastiano Silipo <sebastiano.silipo98@gmail.com>
2025-10-13 17:50:09 +0900

    [#ADD] Add project links from skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,4 +1,57 @@
+import { useState } from "react";
+
 const Skills = () => {
+
+    const [activeTooltip, setActiveTooltip] = useState(null);
+
+    const handleClick = (key) => {
+        setActiveTooltip(activeTooltip === key ? null : key);
+    }
+
+    const skills = [
+        {key: "html", name: "HTML", icon:"html5", projects:[
+            {name: "Front-End Developer Capstone", link: "/courseraCapstone"},
+            {name: "React Project Portfolio", link: "/courseraPortfolio"},
+            {name: "This Portfolio", link: "/personalPortfolio"},
+        ]},
+        {key: "css", name:"CSS", icon:"css3-alt", projects:[
+            {name: "Front-End Developer Capstone", link: "/courseraCapstone"},
+            {name: "React Project Portfolio", link: "/courseraPortfolio"},
+            {name: "This Portfolio", link: "/personalPortfolio"},
+        ]},
+        {key: "js", name:"JavaScript", icon:"js-square", projects:[
+            {name: "Front-End Developer Capstone", link: "/courseraCapstone"},
+            {name: "React Project Portfolio", link: "/courseraPortfolio"},
+            {name: "This Portfolio", link: "/personalPortfolio"},
+            {name: "Weather App", link: "/weatherApp"},
+        ]},
+        {key: "react", name:"React", icon:"react", projects:[
+            {name: "Front-End Developer Capstone", link: "/courseraCapstone"},
+            {name: "React Project Portfolio", link: "/courseraPortfolio"},
+            {name: "This Portfolio", link: "/personalPortfolio"},
+        ]},
+        {key: "java", name:"Java", icon:"java", projects:[
+            {name: "EOLO Blog", link: "/eoloBlog"},
+            {name: "Mendrisio Website", link: "/mendrisio"},
+            {name: "EOLO SEO", link: "/eoloSeo"},
+        ]},
+        {key: "python", name:"Python", icon:"python", projects:[
+            {name: "One-shot Music Genre Classification", link: "/oneshot"},
+        ]},
+        {key: "figma", name:"Figma", icon:"figma", projects:[
+            {name: "Front-End Developer Capstone", link: "/courseraCapstone"},
+            {name: "React Project Portfolio", link: "/courseraPortfolio"},
+        ]},
+        {key: "git", name:"Git", icon:"git", projects:[
+            {name: "Front-End Developer Capstone", link: "/courseraCapstone"},
+            {name: "React Project Portfolio", link: "/courseraPortfolio"},
+            {name: "This Portfolio", link: "/personalPortfolio"},
+            {name: "EOLO Blog", link: "/eoloBlog"},
+            {name: "Mendrisio Website", link: "/mendrisio"},
+            {name: "EOLO SEO", link: "/eoloSeo"},
+        ]}
+    ]
+
     return (
     <div className="col-lg-10 skills-container">
         <div className="col-12">
@@ -6,38 +59,32 @@ const Skills = () => {
         </div>
       
         <div className="grid-skills">
-            <div className="skill-card html">
-                <i className="fa-brands fa-html5 html-icon"></i>
-                <p>HTML</p>
-            </div>
-            <div className="skill-card css">
-                <i className="fa-brands fa-css3-alt css-icon"></i>
-                  <p>CSS</p>
-            </div>
-            <div className="skill-card js">
-                <i className="fa-brands fa-js-square js-icon"></i>
-                <p>JavaScript</p>
-            </div>
-            <div className="skill-card react">
-                <i className="fa-brands fa-react react-icon"></i>
-                <p>React</p>
-            </div>
-            <div className="skill-card java">
-                <i className="fa-brands fa-java java-icon"></i>
-                <p>Java</p>
-            </div>
-            <div className="skill-card python">
-                <i className="fa-brands fa-python python-icon"></i>
-                <p>Python</p>
-            </div>
-            <div className="skill-card figma">
-                <i className="fa-brands fa-figma figma-icon"></i>
-                <p>Figma</p>
-            </div>
-            <div className="skill-card git">
-                <i className="fa-brands fa-git git-icon"></i>
-                <p>Git</p>
-            </div>
+
+            {skills.map(skill => (
+                <div key={skill.key} className={`skill-card ${skill.key}`} onClick={() => handleClick(skill.key)}>
+                    <i className={`fa-brands fa-${skill.icon} ${skill.key}-icon`}></i>
+                    <p>{skill.name}</p>
+
+                    {/* Tooltip */}
+                    {activeTooltip === skill.key && skill.projects.length > 0 && (
+                            
+                        <div className="tooltipProjects bg-white border rounded-lg shadow-lg p-2 z-10">
+                            <b>Projects with {skill.name}</b>
+                            <ul className="space-y-1 projectList">
+                            {skill.projects.map((project, index) => (
+                                <li key={index}>
+                                    <a href={project.link} rel="noopener noreferrer" className="projectLink block py-1 text-sm text-gray-700 rounded">
+                                        {project.name}
+                                    </a>
+                                </li>
+                            ))}
+                            </ul>
+                        </div>
+
+                    )}
+                </div>
+            ))}
+
         </div>
     </div>
     )
