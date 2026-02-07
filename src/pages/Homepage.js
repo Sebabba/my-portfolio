7e88ba6318bd3c98b9525512ea967d7e36b00008
Sebastiano Silipo <sebastiano.silipo98@gmail.com>
2026-02-07 17:05:39 +0100

    Add scroll-mt-20 to page section wrappers

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,19 +7,19 @@ import Contact from '../components/Contact';
 const Homepage = () => {
   return (
     <div>
-      <div id="hero" className="my-6">
+      <div id="hero" className="my-6 scroll-mt-20">
         <Hero />
       </div>
-      <div id="about" className="mt-5">
+      <div id="about" className="mt-5 scroll-mt-20">
         <About />
       </div>
-      <div id="skills">
+      <div id="skills" className="scroll-mt-20">
         <Skills />
       </div>
-      <div id="projects">
+      <div id="projects" className="scroll-mt-20">
         <Projects />
       </div>
-      <div id="contact" className="mb-6">
+      <div id="contact" className="mb-6 scroll-mt-20">
         <Contact />
       </div>
     </div>
